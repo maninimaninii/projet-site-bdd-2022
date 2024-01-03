eb324c056ff0f48b8f351272051839c9be276080
maninimaninii <153753176+maninimaninii@users.noreply.github.com>
2024-01-03 03:01:25 +0100

    Amelioration esthetique

--- listefilms.php
+++ listefilms.php
@@ -1,61 +1,24 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
     <title>Films</title>
     <link rel='stylesheet' href='styl.css'>
-  </head>
-  <body>
-  <header>
-        <div class="wrap">
-            <ul class="navigation">
-                <li><a href="acceuil.html"> Acceuil </a></li>
-                <li><a href="listepalmes.php"> Palmes d'or </a></li>
-                <li><a href="listeprix.php"> Prix</a></li>
-                <li><a href="listelaureats.php"> Lauréats </a></li>
-            </ul>
-        </div>
-      </div>
-    </header>
+    <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+</head>
+<body>
 
-  <section class="coucou">
-   <?php 
-   include_once("fonct.php");
-   $col = getAllFilms(); 
-echo "<br> <br> <br> <br><ul>"; 
-foreach($col as $c){
-    echo"<li> <ul>";
-    foreach($c as $x => $v){
-        echo"<li>"; 
-    echo  $x ." : ". $v ."</li><br>";}
-    echo ' <button class="supprime" onclick="supprimer('."'".$c['Titre du film']."'".')">Supprimer</button> <br>';
-    echo ' <button class="modifie" onclick="modifier('."'".$c['Titre du film']."'".')">Modifier</button> <br>';
-    echo "</ul> </li>";
-    echo "<br> <br> <br> <br>";
-}
-echo "</ul>"; ?>
+<header>
+    <div id="navbar-container"></div>
+</header>
 
-  </section>
-
-  <footer>
-    <a href="insererfilm.html"> Insérer un nouveau film </a>
-  </footer>
-
-  </body>
-</html>
-
-<script>
-function supprimer(nom) {
-  //fonction appellee lors du clic, confirmation de la suppression puis fonctions php pour s'en occuper
-  if (confirm('Voulez-vous vraiment supprimer l élément ' + nom +' ?')) {
-    window.location.href = 'supprimeF.php?nom=' + nom;
-  }
-}
-
-function modifier(nom) {
-  //fonction appellee lors du clic, confirmation de la suppression puis fonctions php pour s'en occuper
-  if (confirm('Voulez-vous vraiment modifier l élément ' + nom +' ?')) {
-    window.location.href = 'modifieF.php?nom=' + nom;
-  }
-}
+<script src="Navbar.js" type="module"></script>
+<script type="module">
+    import Navbar from './Navbar.js';
+    const navbarContainer = document.getElementById('navbar-container');
+    ReactDOM.render(<Navbar />, navbarContainer);
 </script>
+
+</body>
+</html>
